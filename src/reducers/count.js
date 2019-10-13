7d3986ded5dbb3c0529e0eb6456a3e2dfe0d789f
Kenji Mano <whiskeygolfdelta@gmail.com>
2019-10-14 00:11:16 +0900

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,13 @@
+import {INCREMENT,DECREMENT} from '../actions'
+
+const initialState={value:0}
+export default (state=initalState,action)=> {
+    switch(action.type){
+        case INCREMENT:
+            return {vale: state.value+1}
+        case DECREMENT:
+            return {vale: state.value-1}
+        default:
+            return state
+    }
+}
