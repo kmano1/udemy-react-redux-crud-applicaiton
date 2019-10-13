6712959596dc07b9e1dbc6da8262a461bee90ae8
Kenji Mano <whiskeygolfdelta@gmail.com>
2019-10-13 23:16:58 +0900

    learn propTYpe

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-
-// class App extends Component{
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input id="bar" type="text" onClick={()=>{console.log("aaa")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
+import PropTypes from 'prop-types'
 const App=()=>{
   const profiles=[
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {name: "Noname"}
+    {name: "Noname",age: 3}
 
   ]
   return (
@@ -30,8 +19,9 @@ const User=(props)=>{
   return <div>Hi, I am {props.name}! and {props.age} years old</div>
 }
 
-User.defaultProps={
-  age:1
+User.propTypes={
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 export default App;
 
